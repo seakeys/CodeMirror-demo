95095fb81a4236c86386086dc6ccb83faf049c14
陈海飞 <649719596@qq.com>
2020-07-31 13:31:38 +0800

    feat: update

--- codemirror.js
+++ codemirror.js
@@ -3543,7 +3543,6 @@
   // Sync the scrollable area and scrollbars, ensure the viewport
   // covers the visible area.
   function updateScrollTop(cm, val) {
-    debugger
     if (Math.abs(cm.doc.scrollTop - val) < 2) { return }
     if (!gecko) { updateDisplaySimple(cm, {top: val}); }
     setScrollTop(cm, val, true);
@@ -8190,10 +8189,11 @@
   function hiddenTextarea() {
     var te = elt("textarea", null, null, "position: absolute; bottom: -1em; padding: 0; width: 1px; height: 1em; outline: none");
     var div = elt("div", [te], null, "overflow: hidden; position: relative; width: 3px; height: 0px;");
-    // The textarea is kept positioned near the cursor to prevent the
-    // fact that it'll be scrolled into view on input from scrolling
-    // our fake cursor out of view. On webkit, when wrap=off, paste is
-    // very slow. So make the area wide instead.
+    console.log(te)
+    //文本区域保持在光标附近，以防止
+    //事实上，它将滚动到视图的输入从滚动
+    //我们的假光标看不见了。在webkit上，当wrap=off时，粘贴
+    //非常慢。所以把区域变宽。
     if (webkit) { te.style.width = "1000px"; }
     else { te.setAttribute("wrap", "off"); }
     // If border: 0; -- iOS fails to open keyboard (issue #1287)
